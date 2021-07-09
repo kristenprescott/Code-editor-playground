4d6a6d946e440e4cd9308cd0fe54b24dfc12be57
KayPea <prescottdarling@gmail.com>
2021-07-09 16:48:34 -0400

    Current state: fully functional HTML/CSS/JS tabs on one side & result(output)/console tabs on the other. The console tab currently prints each time any change is made in the HTML/CSS/JS tabs, which is not ideal.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import Playground from "@agney/playground";
+/* Why is there a tabs import? https://github.com/agneym/playground#why-is-there-a-reacttabs-import*/
+import "@reach/tabs/styles.css";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const snippet = {
+    markup: `
+<body>
+  <div class="main">
+    Hello, world.
+  </div>
+</body>
+`,
+    css: `
+.main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  width: 100vw;
+  height: 100vh;
+  font-size: 22px;
+  font-weight: bolder;
+  color: yellow;
+}
+`,
+    javascript: `
+console.log("hello, world.")
+
+const div = document.querySelector('div');
+div.style.backgroundColor = 'red';
+`,
+    //     javascript: `import React, { createElement } from "https://cdn.skypack.dev/react";
+    // import ReactDOM from "https://cdn.skypack.dev/react-dom";
+
+    // ReactDOM.render(
+    //   <h1>Hello World</h1>,
+    //   document.getElementById("app")
+    // );`,
+  };
+
+  // const theme = {
+  //   container: {
+  //     borderColor: "",
+  //     minHeight: "20em",
+  //     minWidth: "15em",
+  //   },
+  //   error: {
+  //     background: "",
+  //     color: "",
+  //   },
+  //   console: {
+  //     background: "rgba(0, 0, 0, 1)",
+  //   },
+  //   divider: {
+  //     width: 2,
+  //     background: "",
+  //   },
+  //   editor: {
+  //     fontFamily: `Consolas, Menlo, Monaco, source-code-pro, Courier New, monospace`,
+  //     backgroundColor: ``,
+  //     color: ``,
+  //   },
+  //   tabs: {
+  //     tabHeader: {
+  //       borderBottom: "",
+  //       panelBackground: "",
+  //       background: "",
+  //       color: "",
+  //     },
+  //     tabPanel: {
+  //       phoneHeight: "10em",
+  //     },
+  //     selectedTab: {
+  //       background: "",
+  //       borderBottom: "0.2em solid rgb(0, 0, 0)",
+  //     },
+  //   },
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="playground">
+      <Playground
+        id="playground"
+        initialSnippet={snippet}
+        defaultEditorTab="markup"
+        defaultResultTab="result"
+        transformJs="true"
+        presets={["react"]}
+        // theme="theme"
+      />
     </div>
   );
-}
+};
 
 export default App;
